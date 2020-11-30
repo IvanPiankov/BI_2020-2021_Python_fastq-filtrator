010612e7f6b8875c55841e76c51e2a959c04acdf
IvanPiankov <vanypyankov@gmail.com>
2020-11-30 21:50:01 +0300

    First_final_version_fastq_filter

--- Fastq_filtrator.py
+++ Fastq_filtrator.py
@@ -1,49 +1,47 @@
 import sys
-fasta = str(sys.argv[-1])
-counter = 1
-min_length = 1
-gc_counter = False
+
+
+fasta = str(sys.argv[-1])   # fastq - позиционный аргумент поэтому он
+file_list = sys.argv        # всегда будет последним в списке передаваемых аргументов
+min_length = 1              # Объявляем флаги, для того, чтобы с их использованием запускать те -
+gc_counter = False          # или иные акты в коде
 not_filtered_file = False
 name_flag = False
 
-for i in range(counter, len(sys.argv)):
-    if sys.argv[i] == "--min_length":
-        min_length = sys.argv[i+1]
-        counter = i+1
-    elif sys.argv[i] == "--keep_filtered":
-        not_filtered_file = True
-    elif sys.argv[i] == "--gc_bounds":
-        min_gc = float(sys.argv[i+1])
-        counter = i+1
-        gc_counter = True
-        try:
-            max_gc = float(sys.argv[i+2])
-            counter = i + 2
-        except ValueError:
-            max_gc = min_gc
-            continue
-    elif sys.argv[i] == "--output_base_name":
-        file_names = sys.argv[i+1]
-        counter = i + 1
-        name_flag = True
+if "--min_length" in file_list:         # Идея с if не самый элегантный вариант,
+    i = file_list.index("--min_length") # Но если мы будем искать аргументы через for это будет дольше
+    min_length = file_list[i + 1]       # Пробывал и сравнивал время работы
+if "--keep_filtered" in file_list:      # При помощи if мы пытаемся найти тот или иной флаг из ком.строки
+    not_filtered_file = True
+if "--gc_bounds" in file_list:
+    i = file_list.index("--gc_bounds")
+    min_gc = float(file_list[i + 1])
+    gc_counter = True
+    try:
+        max_gc = float(file_list[i + 2])
+    except ValueError:
+        max_gc = min_gc
+if "--output_base_name" in file_list:
+    i = file_list.index("--output_base_name")
+    file_names = file_list[i + 1]
+    name_flag = True
 
-if name_flag == False:
-    file_names = fasta[:-6]
+if name_flag is False:                  # Проверяем есть ли у нас имя выходного файла
+    file_names = fasta[:-6]             # И выполняем сопутствующие действия
     good_name = file_names + "_passed"
     bad_name = file_names + "_faild"
 else:
     good_name = file_names + "_passed.fastq"
     bad_name = file_names + "_faild.fastq"
 
-if not_filtered_file == True:
-    good_file = open(good_name, "w")
-    bad_file = open(bad_name, "w")
+if not_filtered_file is True:           # Проверяем есть ли у нас флаг для создания файла с
+    good_file = open(good_name, "w")    # С не прошед. фильтр ридами
+    bad_file = open(bad_name, "w")      # Создаем и открываем файлы для записи
 else:
     good_file = open(good_name, "w")
 
 
-
-def filtration_for_lengt(lin1, min_length):
+def filtration_for_lengt (lin1, min_length):    # Функция для прохождения ридами фильтра по длине
     int_length = int(lin1.split("=")[1])
     min_length = int(min_length)
     if int_length >= min_length:
@@ -51,14 +49,15 @@ def filtration_for_lengt(lin1, min_length):
     else:
         return False
 
-def gc_counter_filter(lin2, min_gc, max_gc):
+
+def gc_counter_filter(lin2, min_gc, max_gc):    # Функция для прохождения ридами фильтра по GC%
     min_gc = int(min_gc)
     max_gc = int(max_gc)
     total = len(lin2)
     c = lin2.count("C")
     g = lin2.count("G")
     gc_total = g + c
-    gc_content = (gc_total/total)*100
+    gc_content = (gc_total / total) * 100
     if max_gc == min_gc:
         if min_gc <= gc_total:
             return True
@@ -71,39 +70,35 @@ def gc_counter_filter(lin2, min_gc, max_gc):
             return False
 
 
+def writing_file(lin1, lin2, lin3, lin4, length_filter, gc_filter):   # Функция для записи ридов
+    if length_filter is True and gc_filter is True:             # Запись происходит в разные файлы по результатом
+        good_file.write(lin1 + "\n")                            # Значения флага, которое мы ранее получили
+        good_file.write(lin2 + "\n")
+        good_file.write(lin3 + "\n")
+        good_file.write(lin4 + "\n")
+    elif not_filtered_file is True:
+        bad_file.write(lin1 + "\n")
+        bad_file.write(lin2 + "\n")
+        bad_file.write(lin3 + "\n")
+        bad_file.write(lin4 + "\n")
+    else:
+        pass
+
 
 with open(fasta, "r") as fastq:
-    lin1 = fastq.readline().rstrip("\n")  # reading first string to start cycle
-    while lin1 != "":  # fastq files ends by empty string, and we can use it for stop cycle
-        lin2 = fastq.readline().rstrip("\n")  # reading second line with nucleotide
-        l, k = fastq.readline().rstrip("\n"), fastq.readline().rstrip("\n")  # this strings are not needed for our code, but we should read them
-        length_filter = filtration_for_lengt(lin1, min_length)
-        if  gc_counter == True:
+    lin1 = fastq.readline().rstrip("\n")            # Считываем первую строку для входа в цикл
+    while lin1 != "":                               # Вроде бы каждый fastq файл заканчивается ""
+        lin2 = fastq.readline().rstrip("\n")        # Так мы можем ловить конец файла
+        lin3, lin4 = fastq.readline().rstrip("\n"), fastq.readline().rstrip("\n")
+        length_filter = filtration_for_lengt(lin1, min_length)      # Запуск сторонних функций для
+        if gc_counter is True:                                      # Прохождения различных фильтров
             gc_filter = gc_counter_filter(lin2, min_gc, max_gc)
         else:
             gc_filter = True
-        if not_filtered_file == True:
-            if length_filter == True and gc_filter == True:
-                good_file.write(lin1 + "\n")
-                good_file.write(lin2 + "\n")
-                good_file.write(l + "\n")
-                good_file.write(k + "\n")
-            else:
-                bad_file.write(lin1 + "\n")
-                bad_file.write(lin2 + "\n")
-                bad_file.write(l + "\n")
-                bad_file.write(k + "\n")
-        else:
-            if length_filter == True and gc_filter == True:
-                good_file.write(lin1 + "\n")
-                good_file.write(lin2 + "\n")
-                good_file.write(l + "\n")
-                good_file.write(k + "\n")
-            else:
-                pass
-        lin1 = fastq.readline().rstrip("\n")  # reading line for stop cycle if line is empty
+        writing_file(lin1, lin2, lin3, lin4, length_filter, gc_filter)
+        lin1 = fastq.readline().rstrip("\n")        # Записываем следующую линию, для того, чтобы цикл не ушел в небеса
 
-if not_filtered_file == True:
+if not_filtered_file is True:                       # Обязательно! Закрываем файлы
     good_file.close()
     bad_file.close()
 else:
