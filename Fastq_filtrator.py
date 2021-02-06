8c933243213ddfa88146c78a5e6eded56e0ea5ec
IvanPiankov <vanypyankov@gmail.com>
2021-02-06 22:52:26 +0300

    Second_final_version_fastq_filter

--- Fastq_filtrator.py
+++ Fastq_filtrator.py
@@ -1,106 +1,108 @@
 import sys
 
 
-fasta = str(sys.argv[-1])   # fastq - позиционный аргумент поэтому он
-file_list = sys.argv        # всегда будет последним в списке передаваемых аргументов
-min_length = 1              # Объявляем флаги, для того, чтобы с их использованием запускать те -
-gc_counter = False          # или иные акты в коде
-not_filtered_file = False
-name_flag = False
+fastq_file = str(sys.argv[-1])           # fastq is position file
+file_list = sys.argv
+min_length = 1                           # min Length for filtration
+gc_counter = False                       # flag for gc_counter function
+not_filtered_file = False                # flag for file where non-passed reads would be written
+name_flag = False                        # flag to create name
+max_gc_flag = False                      # flag to find out if the upper limit has been entered for sorting by GC
 
-if "--min_length" in file_list:         # Идея с if не самый элегантный вариант,
-    i = file_list.index("--min_length") # Но если мы будем искать аргументы через for это будет дольше
-    min_length = file_list[i + 1]       # Пробывал и сравнивал время работы
-if "--keep_filtered" in file_list:      # При помощи if мы пытаемся найти тот или иной флаг из ком.строки
+if "--min_length" in file_list:          # parsing and transformation variables
+    i = file_list.index("--min_length")
+    min_length = file_list[i + 1]
+    min_length = int(min_length)
+if "--keep_filtered" in file_list:
     not_filtered_file = True
 if "--gc_bounds" in file_list:
     i = file_list.index("--gc_bounds")
     min_gc = float(file_list[i + 1])
+    min_gc = int(min_gc)
     gc_counter = True
     try:
         max_gc = float(file_list[i + 2])
+        max_gc = int(max_gc)
+        max_gc_flag = True
     except ValueError:
         max_gc = min_gc
 if "--output_base_name" in file_list:
     i = file_list.index("--output_base_name")
-    file_names = file_list[i + 1]
+    file_name = file_list[i + 1]
     name_flag = True
 
-if name_flag is False:                  # Проверяем есть ли у нас имя выходного файла
-    file_names = fasta[:-6]             # И выполняем сопутствующие действия
-    good_name = file_names + "_passed"
-    bad_name = file_names + "_faild"
+# Name new files
+if name_flag is False:
+    file_name = fastq_file[:-6]
+    good_name = file_name + "__passed.fastq"
+    bad_name = file_name + "__failed.fastq"
 else:
-    good_name = file_names + "_passed.fastq"
-    bad_name = file_names + "_faild.fastq"
-
-if not_filtered_file is True:           # Проверяем есть ли у нас флаг для создания файла с
-    good_file = open(good_name, "w")    # С не прошед. фильтр ридами
-    bad_file = open(bad_name, "w")      # Создаем и открываем файлы для записи
-else:
-    good_file = open(good_name, "w")
+    good_name = file_name + "_passed.fastq"
+    bad_name = file_name + "_failed.fastq"
 
 
-def filtration_for_lengt (lin2, min_length):    # Функция для прохождения ридами фильтра по длине
-    int_length = len(lin2)
-    min_length = int(min_length)
+# Filter line by length
+def filtration_for_length(line_2, min_length):
+    int_length = len(line_2)
     if int_length >= min_length:
         return True
     else:
         return False
 
 
-def gc_counter_filter(lin2, min_gc, max_gc):    # Функция для прохождения ридами фильтра по GC%
-    min_gc = int(min_gc)
-    max_gc = int(max_gc)
-    total = len(lin2)
-    c = lin2.count("C")
-    g = lin2.count("G")
+# Filter line by GC%
+def gc_counter_filter(line_2, min_gc, max_gc, max_gc_flag):
+    total = len(line_2)
+    c = line_2.count("C")
+    g = line_2.count("G")
     gc_total = g + c
     gc_content = (gc_total / total) * 100
-    if max_gc == min_gc:
-        if min_gc <= gc_total:
+    if max_gc_flag:
+        if min_gc <= gc_content <= max_gc:
             return True
         else:
             return False
     else:
-        if min_gc <= gc_total <= max_gc:
+        if min_gc <= gc_content:
             return True
         else:
             return False
 
 
-def writing_file(lin1, lin2, lin3, lin4, length_filter, gc_filter):   # Функция для записи ридов
-    if length_filter is True and gc_filter is True:             # Запись происходит в разные файлы по результатом
-        good_file.write(lin1 + "\n")                            # Значения флага, которое мы ранее получили
-        good_file.write(lin2 + "\n")
-        good_file.write(lin3 + "\n")
-        good_file.write(lin4 + "\n")
-    elif not_filtered_file is True:
-        bad_file.write(lin1 + "\n")
-        bad_file.write(lin2 + "\n")
-        bad_file.write(lin3 + "\n")
-        bad_file.write(lin4 + "\n")
-    else:
-        pass
+# Write lines in new files
+def writing_file(line_1, line_2, line_3, line_4, length_filter, gc_filter):
+    if length_filter and gc_filter:
+        good_file.write(line_1 + "\n")
+        good_file.write(line_2 + "\n")
+        good_file.write(line_3 + "\n")
+        good_file.write(line_4 + "\n")
+    elif not_filtered_file:
+        bad_file.write(line_1 + "\n")
+        bad_file.write(line_2 + "\n")
+        bad_file.write(line_3 + "\n")
+        bad_file.write(line_4 + "\n")
 
 
-with open(fasta, "r") as fastq:
-    lin1 = fastq.readline().rstrip("\n")            # Считываем первую строку для входа в цикл
-    while lin1 != "":                               # Вроде бы каждый fastq файл заканчивается ""
-        lin2 = fastq.readline().rstrip("\n")        # Так мы можем ловить конец файла
-        lin3, lin4 = fastq.readline().rstrip("\n"), fastq.readline().rstrip("\n")
-        length_filter = filtration_for_lengt(lin2, min_length)      # Запуск сторонних функций для
-        if gc_counter is True:                                      # Прохождения различных фильтров
-            gc_filter = gc_counter_filter(lin2, min_gc, max_gc)
-        else:
-            gc_filter = True
-        writing_file(lin1, lin2, lin3, lin4, length_filter, gc_filter)
-        lin1 = fastq.readline().rstrip("\n")        # Записываем следующую линию, для того, чтобы цикл не ушел в небеса
+# Read fastq files
+def fastq_reader(*args):
+    with open(fastq_file, "r") as fastq:
+        line_1 = fastq.readline().rstrip("\n")
+        while line_1 != "":                           # An empty line will mean the end of the file and we end the loop
+            line_2 = fastq.readline().rstrip("\n")  # Reading line with nucleotide sequence location
+            line_3, line_4 = fastq.readline().rstrip("\n"), fastq.readline().rstrip("\n")  # Read the remaining lines
+            length_filter = filtration_for_length(line_2, min_length)         # Launching third-party functions
+            if gc_counter:                                                             # If we have True flag
+                gc_filter = gc_counter_filter(line_2, min_gc, max_gc, max_gc_flag)     # function sort by GC%
+            else:
+                gc_filter = True                                                       # Otherwise we do not sort by GC%
+            writing_file(line_1, line_2, line_3, line_4, length_filter, gc_filter)
+            line_1 = fastq.readline().rstrip("\n")      # Reading new string line 1
 
-if not_filtered_file is True:                       # Обязательно! Закрываем файлы
-    good_file.close()
-    bad_file.close()
+
+# Create new files
+if not_filtered_file:
+    with open(good_name, "w") as good_file, open(bad_name, "w") as bad_file:
+        fastq_reader(good_file, bad_file)
 else:
-    good_file.close()
-
+    with open(good_name, "w") as good_file:
+        fastq_reader(good_file)
